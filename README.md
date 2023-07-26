abd01d80bd79ad63baede6a40dbd8050e325e1d1
Fernando Perry <fernandoperry1234@gmail.com>
2023-07-26 21:08:11 +0700

    changed to use num and min max

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ cargo build --release
 
 ## USAGE
 ```swayconf
-bindgesture swipe:right exec $HOME/.config/sway/script/scwork prev
-bindgesture swipe:left exec $HOME/.config/sway/script/scwork next 
+bindgesture swipe:right exec $HOME/.config/sway/script/scwork prev -min 1 -max 10
+bindgesture swipe:left exec $HOME/.config/sway/script/scwork next -min 1 -max 10
 ```
 
 - EXAMPLE on normal shell
@@ -20,14 +20,16 @@ scwork prev
 # for next
 scwork n
 scwork next
+# using min and max
+scwork n -max 5 # will not move to workspace bigger than 5
+scwork p -min 5 # will not move to workspace less than 5
 ```
 
 ## LIMITATION
-- for now it only support 10 workspaces
 - when using prev on workspace 1 and next on workspace 10 will do nothing
-- only support workspaces with 1-10 name
 
 ## BEHAVIOR
 the behavior of this custom prog/script are:
 - at ``next`` it will +1 the current workspace name
 - at ``prev`` it will -1 the current workspace name
+- the default min and max value are 1 and 10
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use std::process::{Command, Output};
 
 #[derive(Deserialize, Serialize)]
 struct WorkspaceData {
-    name: String,
+    num: usize,
     focused: bool,
+    output: String,
 }
 
 enum ProgMode {
@@ -14,6 +15,7 @@ enum ProgMode {
     Next,
 }
 
+// TODO: add min & max
 fn spawn_process(program: &str, args: Vec<&str>, capture_output: bool) -> Result<String, ()> {
     match capture_output {
         true => {
@@ -57,23 +59,20 @@ fn get_args() -> Vec<String> {
     return tmp_vec;
 }
 
-fn calculate_workspaces(mode: ProgMode, workspaces: Vec<WorkspaceData>) -> Result<usize, ()> {
+fn calculate_workspaces(mode: ProgMode, workspaces: Vec<WorkspaceData>, min: usize, max: usize) -> Result<usize, ()> {
     for workspace in workspaces {
         if workspace.focused == true {
-            let mut num: usize = match workspace.name.trim().parse() {
-                Ok(val) => val,
-                Err(_) => return Err(()),
-            };
+            let mut num: usize = workspace.num;
             match mode {
                 ProgMode::Prev => {
-                    if num == 1 {
+                    if num <= min {
                         return Err(());
                     }
                     num -= 1;
                     return Ok(num);
                 }
                 ProgMode::Next => {
-                    if num == 10 {
+                    if num >= max {
                         return Err(());
                     }
                     num += 1;
@@ -87,14 +86,35 @@ fn calculate_workspaces(mode: ProgMode, workspaces: Vec<WorkspaceData>) -> Resul
 
 fn main() {
     let user_args: Vec<String> = get_args();
+    let mut min:usize = 1;
+    let mut max:usize = 10;
     if user_args.len() < 2 {
         eprintln!("ERR: Not enought args.");
         process::exit(1);
+    }
+    for idx in 0..user_args.len() {
+        match &user_args[idx][..] {
+            "-min" => {
+                if idx+2 > user_args.len() {
+                    process::exit(1);
+                }
+                min = user_args[idx+1].trim().parse().unwrap_or(1);
+            },
+            "-max" => {
+                if idx+2 > user_args.len() {
+                    process::exit(1);
+                }
+                max = user_args[idx+1].trim().parse().unwrap_or(10);
+            },
+            _ => {}
+
+        }
     }
 
     let progs: &str = "swaymsg";
     let args_get_workspaces: Vec<&str> = vec!["-t", "get_workspaces"];
     let mut args_set_workspaces: Vec<&str> = vec!["workspace"];
+    let workspace_num: usize;
     let json_output: String = match spawn_process(progs, args_get_workspaces, true) {
         Ok(val) => val,
         Err(_) => {
@@ -111,29 +131,26 @@ fn main() {
     };
     match &user_args[1][..] {
         "p" | "prev" => {
-            let workspace_num: usize = match calculate_workspaces(ProgMode::Prev, json_data) {
+            workspace_num = match calculate_workspaces(ProgMode::Prev, json_data, min, max) {
                 Ok(val) => val,
                 Err(_) => {
                     eprintln!("ERR: Failed to calculate the prev workspace.");
                     process::exit(1);
                 }
             };
-            let workspace_num_str: &str = &workspace_num.to_string();
-            args_set_workspaces.push(workspace_num_str);
-            spawn_process(progs, args_set_workspaces, false).unwrap();
         }
         "n" | "next" => {
-            let workspace_num: usize = match calculate_workspaces(ProgMode::Next, json_data) {
+            workspace_num = match calculate_workspaces(ProgMode::Next, json_data, min, max) {
                 Ok(val) => val,
                 Err(_) => {
                     eprintln!("ERR: Failed to calculate the next workspace.");
                     process::exit(1);
                 }
             };
-            let workspace_num_str: &str = &workspace_num.to_string();
-            args_set_workspaces.push(workspace_num_str);
-            spawn_process(progs, args_set_workspaces, false).unwrap();
         }
-        _ => {}
+        _ => process::exit(1)
     }
+    let workspace_num_str: &str = &workspace_num.to_string();
+    args_set_workspaces.push(workspace_num_str);
+    spawn_process(progs, args_set_workspaces, false).unwrap();
 }
